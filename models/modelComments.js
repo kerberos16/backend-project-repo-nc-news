395a7a77a028e377fa3c0f5dc06c38d64e39e370
Sizen Lyutfi <sizen.ertan@gmail.com>
2022-07-07 12:23:05 +0100

    Completed Task 12

--- models/modelComments.js
+++ models/modelComments.js
@@ -12,3 +12,17 @@ exports.insertComment = (username, article_id, body) => {
     })
 }
 
+
+exports.removeComment =  (comment_id) => {
+    return connection
+    .query ('DELETE FROM comments WHERE comment_id =$1;', [comment_id])
+    .then((rows) => {
+        if(rows.rowCount === 0) {
+            return Promise.reject({
+              status:404,
+              msg: "Page not found: Comment does not exist"
+            })
+    } else return rows
+    
+})};
+
